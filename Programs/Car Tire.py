bb12ba5e4908f410e5f6289be86cdfbe7ab5b9fc
smi15127 <smi15127@byui.edu>
2024-09-25 10:44:45 -0600

    edited

---
+++ Programs/Car Tire.py
@@ -0,0 +1,93 @@
+"""
+{ James Smith, Programming w/ Functions 0745 - 0845
+{ Week of 22 SEPT 24
+{ Calculate the volume of space inside a tire
+"""
+import math
+import csv
+
+# Input
+InputIntro = """
+The size of a car tire in the United States is represented with three numbers like this: 205/60R15. 
+The first number is the width of the tire in millimeters(205). The second number is the aspect ratio(60). 
+The third number is the diameter in inches of the wheel that the tire fits.
+(15, the R denotes a tire of radial construction)
+Use this as reference as you answer the following questions to measure the volume capacity of your tire.
+"""
+print(InputIntro)
+mmWidth = int(input("What is the width of your tire? Please provide in Milimeters: "))
+AspectRatio = int(input("What is the aspect ratio of your tire? "))
+Diameter = int(input("What is the diameter of your wheel? Please provide in inches: "))
+
+# Process
+
+#Initializing datetime.()
+from datetime import datetime
+
+#   Defining what day and time it is in order to append
+current_date = datetime.now().date()
+
+TireVolume = round(float((math.pi * (mmWidth ** 2) * AspectRatio * (mmWidth * AspectRatio + 2540 * Diameter)) / 10000000000), 2)
+
+new_data = [current_date, mmWidth, AspectRatio, Diameter, TireVolume]
+
+file = 'c:\\Users\\it_is\\Documents\\School\\Fall 2024\\CSE 111\\Programs\\Volumes.txt' # The filepath of the csv file
+
+print("Now opening Volumes.txt") # Lets the user know the file is being opened.
+print()
+
+# Mode for file manipulation can include rwa, or Read, Write, and Append. 
+try:
+    with open(file, 'r', newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+        # This argument will attempt to skip the header if present, which it will be.
+        try:
+            next(csvreader)  # Skip header row if present
+        
+        except StopIteration:
+        # Action in the case that the file is empty
+            pass
+
+
+        data = list(csvreader)
+        if len(data) == 0:
+            print ("There is no data currently present in this file.")
+        else:
+            print("Existing data in file:")
+            # The for loop ensures the file loops through all available rows.
+            for row in data:
+                current_date, mmWidth, AspectRatio, Diameter, TireVolume = row
+                print(row)
+
+except FileNotFoundError:
+    # File doesn't exist yet
+    print(f"The file {file} does not exist. It is now created.")
+
+
+print()
+print("Would you like to append the new information to the file?")
+choice = input("If yes, enter yes. If no, enter no: ")
+choice = choice.capitalize()
+print()
+
+if choice == 'Yes':
+    with open(file, 'a', newline='') as csvfile:
+        # New is being written to the file
+        writer = csv.writer(csvfile)
+        writer.writerow(new_data)
+        print ("Data on Volumes.txt has been updated.")
+
+    print()
+    print("Now closing Volumes.txt")
+    csvfile.close()
+    print()
+elif choice == 'No':
+    print("Volumes.txt is now closed, thank you and have a good day.")
+
+else:
+    print("Your choice was not valid. Please enter either yes, or no.")
+
+"""
+Conclusion section
+"""
--- Programs/Lesson 02 Prove - Discounts.py
+++ Programs/Lesson 02 Prove - Discounts.py
@@ -6,16 +6,52 @@
 
 # Input
 
-# Process
+# Get subtotal
+SubTotal = float(input("What is the subtotal of today's purchase? "))
+#TodayAsk = input("What is the current day today? ")
+#TodayAskC = TodayAsk.capitalize()
+
+
+#Initializing datetime.()
 from datetime import datetime
 
-# Get the current local date and time
-current_time = datetime.now()
+#   Defining what day and time it is, then getting the day of the week with .weekday(),
+# where monday is 0, and progressing until sunday is 6. 
+current_time = datetime.now().date()
+CurrentDay = current_time.weekday()
 
-print("Current local date and time:", current_time)
+# Process
+def WhenisToday(CurrentDay,TodayAskC): #,TodayAskC
+     # Check if it's Tuesday or Wednesday either by current day or user input
+    if CurrentDay == 1:  # Tuesday check - or TodayAskC == "Tuesday"
+        return 1
+    elif CurrentDay == 2:  # Wednesday check -  or TodayAskC == "Wednesday"
+        return 1
+    else:
+        return 0
+
+
+DateCheck = WhenisToday(CurrentDay) #,TodayAskC
+
+SubTotalFinal = SubTotal
+if DateCheck == 1 and SubTotal >= 50.00:
+    SubTotalFinal = SubTotal - (SubTotal * .1)
+else:
+    SubTotalFinal = SubTotal
+
+Total = SubTotalFinal * 1.06
+SalesTax = SubTotalFinal * 0.06
+DiscountAmount = SubTotalFinal - SubTotal
+
 
 # Output
-
+print()
+print (f"The total amount owed amounts to ${Total:.2f}.")
+print (f"The sales tax applied to this purchase is ${SalesTax:.2f}.")
+print (f"The discount amount for this purchase is ${DiscountAmount:.2f}.")
+print()
 """
-Conclusion section
+   I wrote the program in such a way that there *is* a prompt for the day in question in order to override the 
+day recieved from the datetime check. This allowed me to troubleshoot to see if the program works properly.
+I finished the program and it should work as intended regardless of the current day.
 """
